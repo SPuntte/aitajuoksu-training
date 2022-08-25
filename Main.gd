739dcb85538b2bbbf421e0a82f69046a35b0a4b0
Pontus Lundström <pontus.lundstrom@gmail.com>
2022-08-25 18:10:30 +0300

    Add session 1 content

---
+++ Main.gd
@@ -0,0 +1,14 @@
+extends Spatial
+
+# Player movement
+var run_speed: float = 8.0
+var jump_length: float = 5.5
+var jump_height: float = 2.0
+
+# References to nodes
+onready var player = $Player
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	player.setup_jump(jump_length, jump_height, run_speed)
