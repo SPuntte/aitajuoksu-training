33a17d4181b41503a2accfeacb15dab0ea6cc935
Pontus Lundström <pontus.lundstrom@gmail.com>
2022-09-01 16:18:41 +0300

    Detach camera from player

--- Main.gd
+++ Main.gd
@@ -7,8 +7,15 @@ var jump_height: float = 2.0
 
 # References to nodes
 onready var player = $Player
+onready var camera_pivot = $CameraPivot
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player.setup_jump(jump_length, jump_height, run_speed)
+
+
+func _process(delta):
+	# Attach camera to the ground at player position
+	camera_pivot.translation = player.translation
+	camera_pivot.translation.y = 0
