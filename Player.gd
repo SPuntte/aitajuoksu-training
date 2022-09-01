3bc2d90c396225e017aed490348a776894ff4364
Pontus Lundström <pontus.lundstrom@gmail.com>
2022-09-01 11:00:30 +0300

    Fix jump

--- Player.gd
+++ Player.gd
@@ -42,7 +42,7 @@ func _physics_process(delta):
 		sideways -= 1.0
 	
 	# Jumping
-	if Input.is_action_just_pressed("jump"):
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_speed
 	
 	velocity.y -= gravity * delta
