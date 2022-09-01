302c4c24c6cd62a808074ef5664a809e5aabae14
Pontus Lundström <pontus.lundstrom@gmail.com>
2022-09-01 11:07:33 +0300

    Implement simple collision detection

--- Player.gd
+++ Player.gd
@@ -49,3 +49,11 @@ func _physics_process(delta):
 	velocity.x = sideways * sidestep_speed
 	velocity.z = -run_speed
 	velocity = move_and_slide(velocity, Vector3.UP)
+	
+	# Detect collisions
+	for index in range(get_slide_count()):
+		var collision = get_slide_collision(index)
+		var collision_object = collision.collider as CollisionObject
+		if collision_object.collision_layer & 4:
+			# We're colliding with an obstacle
+			get_tree().reload_current_scene()
